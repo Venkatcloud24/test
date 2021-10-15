70af8bd8afdab283e485ac09a63f444d9bffebb1
Venkat <venkatcloud@gmail.com>
2021-10-15 12:12:07 +0530

    ok

--- src/Comm/FirstClass.java
+++ src/Comm/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to core JAVA");
 		System.out.println("Go back to Eclipse");
+		System.out.println("BranchX changes");
 
 	}
 
